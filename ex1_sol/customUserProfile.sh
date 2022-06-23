e0e536f7a23d0be0d5b618d0cee4bc6767d3b8f4
DannyVol <69720882+DannyVol@users.noreply.github.com>
2022-06-23 18:19:37 +0300

    Update customUserProfile.sh
    opsss....

--- ex1_sol/customUserProfile.sh
+++ ex1_sol/customUserProfile.sh
@@ -1,7 +1,1 @@
-echo Hello $USER
 
-/usr/lib/update-notifier/apt-check --human-readable
-
-if [[ -f "~/.token" && 'stat -c %a' != 600 ]] ; then
-    echo 'Warning: .token file has too open permissions'
-fi
