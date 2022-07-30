800a608febb2d868ec2d40110cf02ea6398ef23e
alonit <alon.itach@mail.huji.ac.il>
2022-07-30 16:25:17 +0300

    ex2 solution

--- ex2_sol/availabilityAgent.sh
+++ ex2_sol/availabilityAgent.sh
@@ -1,11 +1,25 @@
 TEST_PERIODICITY=5
 
+function test_host {
+  local TESTED_HOST=$1
+
+
+  RESULT=0
+  if ping -c 1 "$TESTED_HOST"
+  then
+    RESULT=1
+  fi
+  TEST_TIMESTAMP="$(date +%s%N)"
+
+  echo "Test result for $TESTED_HOST is $PING_LATENCY at $TEST_TIMESTAMP"
+  curl -X POST 'http://localhost:8086/write?db=hosts_metrics' --data-binary "availability_test,host=$TESTED_HOST value=$RESULT $TEST_TIMESTAMP"
+}
+
 while true
 do
-    # your implementation here
+  while read TESTED_HOST; do
+    test_host "$TESTED_HOST"
+  done < hosts
 
-    sleep $TEST_PERIODICITY
+  sleep "$TEST_PERIODICITY"
 done
-
-
-
