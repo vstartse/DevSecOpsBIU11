985b28bdf8447cda7061e4d09f266bf4b0678518
alonit <alon.itach@mail.huji.ac.il>
2022-07-30 16:20:14 +0300

    ex1 solution

---
+++ ex1_sol/customUserProfile-solution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Hello" $USER
+
+/usr/lib/update-notifier/apt-check --human-readable
+
+TOKEN_PATH=/home/$USER/.token
+if [[ -f "$TOKEN_PATH" ]]; then
+  PERM=$(stat -c "%a" "$TOKEN_PATH")
+  if [[ $PERM != "600" ]]; then
+    echo "Warning: .token file has too open permissions"
+  fi
+fi
